8d00d8fe043aca3c81680f9581a9513328f79f9a
Blas Fernández <elite.atencionalcliente@gmail.com>
2025-05-17 16:50:46 +0200

    RanaOS alpha 4

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -11,25 +11,55 @@ int strcmp(const char* a, const char* b) {
 	return (*a - *b);
 }
 
+char* substr(const char* str, int start, int length = -1) {
+    static char buffer[256]; // Ajusta el tamaño según tus necesidades
+    int i = 0;
+
+    if (length == -1) {
+        // Copia desde `start` hasta el final del string
+        while (str[start + i] != '\0' && i < 255) {
+            buffer[i] = str[start + i];
+            i++;
+        }
+    } else {
+        // Copia desde `start` hasta `start + length`
+        while (i < length && str[start + i] != '\0') {
+            buffer[i] = str[start + i];
+            i++;
+        }
+    }
+
+    buffer[i] = '\0';
+    return buffer;
+}
+
+void runcommand(char* s) {
+	if(!strcmp(s, "help")) {
+		Console::write("RanaOS alpha 4 - Help\n");
+		Console::write("  help >> Show this list.\n");
+		Console::write("  version >> Show the version of the current release of RanaOS.\n");
+		Console::write("  echo [text] >> Print text to the console\n");
+	} else if(!strcmp(s, "version")) {
+		Console::write("RanaOS alpha 3\nLicensed with GNU GPL v3.\n");
+	} else if(!strcmp(substr(s, 0, 5), "echo ")) {
+		Console::write(substr(s, 5));
+		Console::putChar('\n');
+	} else {
+		Console::write("Unknown Command. Use 'help' to get a list of commands.\n");
+	}
+}
+
 static char linebuf[256];
 
 extern "C" void kmain() {
 	Console::clearScreen();
-	Console::write("RanaOS alpha 3\nUse 'help' for getting a list of commands.\n");
+	Console::write("RanaOS alpha 4\nUse 'help' for getting a list of commands.\n");
 
 	while (1) {
 		Console::write("\n$- ");
 		char* s = Console::readLine(linebuf, sizeof(linebuf));
 		
-		if(!strcmp(s, "help")) {
-			Console::write("RanaOS alpha 3 - Help\n");
-			Console::write("  help >> Show this list.\n");
-			Console::write("  version >> Show the version of the current release of RanaOS.\n");
-		} else if(!strcmp(s, "version")) {
-			Console::write("RanaOS alpha 3\nLicensed with GNU GPL v3.\n");
-		} else {
-			Console::write("Unknown Command. Use 'help' to get a list of commands.\n");
-		}
+		runcommand(s);		
 	}
 }
 
