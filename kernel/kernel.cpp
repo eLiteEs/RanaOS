346f99f4cc7b7b5f90f677a669d1a5b6a087eb14
eLiteEs <elite.atencionalcliente@gmail.com>
2025-08-21 12:29:38 +0200

    Updated clock command

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -611,6 +611,51 @@ const char* read_file_from_meta(char* name) {
     return nullptr;
 }
 
+void clock() {
+ 	Console::clearScreen();
+	
+	VGraphics::fillRect(0, 0, 1920, 1080, 0xeeeeee);    
+
+	int midX = 1920 / 2;
+	int midY = 1080 / 2;
+
+	VGraphics::draw_circle(midX, midY, 253, 0x0);
+	VGraphics::draw_circle(midX, midY, 251, 0x0);
+	VGraphics::draw_circle(midX, midY, 252, 0x0);
+	
+	do {
+	    	VGraphics::fill_circle(midX, midY, 250, 0xffffff);	
+
+	    	int hour = getHour();
+	    	int minutes = getMinute();
+	    	int seconds = getSecond();   
+	
+	    	int hoursAngle = 90 - ((hour % 12) * 30 + minutes / 2);
+	    	int minutesAngle = 90 - (minutes * 6);
+	    	int secondsAngle = 90 - (seconds * 6);
+
+	    	VGraphics::draw_line_adv(midX, midY, 100, hoursAngle, 0x0);
+	    	VGraphics::draw_line_adv(midX, midY, 170, minutesAngle, 0x0);
+	    	VGraphics::draw_line_adv(midX, midY, 200, secondsAngle, 0x0); 
+
+	    	for (int h = 0; h < 12; h++) {
+    			int angle = 90 - h * 30;   // cada hora 30°, 12h = arriba
+    			int outer = 250;           // radio exterior
+    			int inner = 230;           // radio interior
+
+    			int x_outer = midX + (int)(VGraphics::cos_lookup(angle) * outer);
+    			int y_outer = midY - (int)(VGraphics::sin_lookup(angle) * outer);
+
+    			int x_inner = midX + (int)(VGraphics::cos_lookup(angle) * inner);
+    			int y_inner = midY - (int)(VGraphics::sin_lookup(angle) * inner);
+
+    			VGraphics::draw_line(x_outer, y_outer, x_inner, y_inner, 0x000000);
+		} 
+
+	    	wait_ms(500);
+	} while(true);
+}
+
 extern "C" void start_so();
 
 // Ejecutar comandos
@@ -846,48 +891,7 @@ void runcommand(char* s, bool auth) {
     } else if(!strcmp(s, "start")) {
         start_so(); // Llamar a la función de inicio del SO
     } else if(!strcmp(s, "clock")) {
-    	Console::clearScreen();
-	
-	VGraphics::fillRect(0, 0, 1920, 1080, 0xeeeeee);    
-
-	int midX = 1920 / 2;
-	int midY = 1080 / 2;
-
-	VGraphics::draw_circle(midX, midY, 253, 0x0);
-	VGraphics::draw_circle(midX, midY, 251, 0x0);
-	VGraphics::draw_circle(midX, midY, 252, 0x0);
-	
-	do {
-	    VGraphics::fill_circle(midX, midY, 250, 0xffffff);	
-
-	    int hour = getHour();
-	    int minutes = getMinute();
-	    int seconds = getSecond();   
-	
-	    int hoursAngle = 90 - ((hour % 12) * 30 + minutes / 2);
-	    int minutesAngle = 90 - (minutes * 6);
-	    int secondsAngle = 90 - (seconds * 6);
-
-	    VGraphics::draw_line_adv(midX, midY, 100, hoursAngle, 0x0);
-	    VGraphics::draw_line_adv(midX, midY, 170, minutesAngle, 0x0);
-	    VGraphics::draw_line_adv(midX, midY, 200, secondsAngle, 0x0); 
-
-	    for (int h = 0; h < 12; h++) {
-    		int angle = 90 - h * 30;   // cada hora 30°, 12h = arriba
-    		int outer = 250;           // radio exterior
-    		int inner = 230;           // radio interior
-
-    		int x_outer = midX + (int)(VGraphics::cos_lookup(angle) * outer);
-    		int y_outer = midY - (int)(VGraphics::sin_lookup(angle) * outer);
-
-    		int x_inner = midX + (int)(VGraphics::cos_lookup(angle) * inner);
-    		int y_inner = midY - (int)(VGraphics::sin_lookup(angle) * inner);
-
-    		VGraphics::draw_line(x_outer, y_outer, x_inner, y_inner, 0x000000);
-	    } 
-
-	    wait_ms(500);
-	} while(true);
+    	clock();
     } else {
 	Console::write("Unknown command \"");
 	Console::write(s);
